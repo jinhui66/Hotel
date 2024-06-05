4fcf7e7ba722c25306a5e1014249f504a0dbec8b
jinhui66 <1303101041@qq.com>
2024-06-05 11:53:21 +0800

    login

--- blueprints/menu.py
+++ blueprints/menu.py
@@ -6,7 +6,7 @@ from exts import db,mail
 import hashlib
 import json
 from models.forms import RegisterForm,LoginForm,ForgotForm
-from models.table import User
+from models.table import User, Admin
 import os
 import time
 
@@ -16,9 +16,37 @@ bp = Blueprint('menu',__name__,url_prefix="/")
 def login():
     return render_template('login.html')
 
+
 @bp.route('/login_action',methods=['GET','POST'])
 def login_action():
     if request.method == 'GET':
+        pass
+    else:
+        type = request.form.get('type')
+        form = LoginForm(request.form)
+        if form.validate():
+            email = form.email.data
+            password = form.password.data
+            if type == 'user':
+                get_user = User.query.filter_by(email=email).first()
+                if get_user is not None:
+                    session['user_id'] = get_user.id
+                    session['type'] = 'user'
+                    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                    print(password)
+                    if(get_user.password == password):
+                        print(1)
+                        return jsonify({'status':'success', 'message':''})
+
+            else:
+                print('admin')
+                get_admin = Admin.query.filter_by(email=email).first()
+                if get_admin is not None:
+                    session['user_id'] = get_admin.id
+                    session['type'] = 'admin'
+                    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                    if(get_admin.password == password):
+                        return jsonify({'status': 'success', 'message':''})
         return jsonify({'status':'', 'message':'账号或密码错误'})
 
 @bp.route('/',methods=['GET','POST'])
